ac5ad93a540f159aa39624c9af223c9c4039b76a
ZYQiao <zhaoyuqiao97@gmail.com>
2021-05-04 13:59:32 +0200

    version3

---
+++ face_recognition_service/fr_service/face_recognition_native_api/face_api.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author   : Ecohnoch(xcy)
+# @File     : face_api.py
+# @Function : TODO
+
+from .face_score import FaceScore
+from . import configs as configs
+
+def get_fs_object():
+    return FaceScore(params=configs.face_recognition_params)
+
+def face_recognition(fs, face_path1, face_path2):
+    # fs = FaceScore(params=configs.face_recognition_params)
+    return fs.cal_score(face_path1, face_path2)
+
+if __name__ == '__main__':
+    face1 = '/Users/ecohnoch/Desktop/face_service/face_score/tmp/0006_01.jpg'
+    face2 = '/Users/ecohnoch/Desktop/face_service/face_score/tmp/0007_01.jpg'
+    score = face_recognition(face1, face2)
+    print(score)
