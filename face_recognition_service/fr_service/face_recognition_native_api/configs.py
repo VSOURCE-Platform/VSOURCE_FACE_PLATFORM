4b842ea39fd08d5cee5cefa79e40b73a7a9284aa
Ecohnoch <542305306@qq.com>
2021-05-17 23:22:27 +0800

    Version 1.2.0

--- face_recognition_service/fr_service/face_recognition_native_api/configs.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author   : Ecohnoch(xcy)
-# @File     : configs.py
-# @Function : TODO
-
-import os
-
-face_recognition_params = os.path.join( os.path.dirname(__file__), 'ckpt')
--- face_service_deploy/my_mongo/setup.sh
+++ face_service_deploy/my_mongo/setup.sh
@@ -2,4 +2,5 @@
 mongo -u my_root -p my_123456 <<EOF
 use face_recognition;
 db.createUser({user:'xcy',pwd:'xcy123456',roles:[{role:'readWrite',db:'face_recognition'}]});
+db.users.insertOne({'username':'xxxxxx@qq.com', 'password': 'xxxxxx123', 'permission': 'NORMAL'});
 EOF
